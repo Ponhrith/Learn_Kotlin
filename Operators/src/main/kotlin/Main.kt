95e0f2576d40867c5a0177a047817a3661be608e
Ponhrith <ponhrith.nhep@gmail.com>
2024-03-04 17:52:15 +0800

    TST-Operators

--- Operators/src/main/kotlin/Main.kt
+++ Operators/src/main/kotlin/Main.kt
@@ -1,11 +1,15 @@
 fun main() {
     var x = 5
-    val y = 3
-
+    val y = 3.0
 
     println("x + y = ${x + y}")
     println("x - y = ${x - y}")
     println("x * y = ${x * y}")
     println("x / y = ${x / y}")
     println("x % y = ${x % y}")
+
+    var result = x + y
+
+    result = result + 2
+    println("result = $result")
 }
