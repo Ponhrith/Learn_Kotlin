30416144a59154af94be5b73918ae479ba4705fb
Ponhrith <ponhrith.nhep@gmail.com>
2024-03-04 12:08:15 +0800

    TST-val

--- Variables/Variable/src/main/kotlin/Main.kt
+++ Variables/Variable/src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 fun main() {
     var userName: String = "Pony"
     userName = "Kiddo"
-    println("Hello $userName!")
+
+    var age: Int = 19
+
+    println("Hello $userName your age is $age")
 }
