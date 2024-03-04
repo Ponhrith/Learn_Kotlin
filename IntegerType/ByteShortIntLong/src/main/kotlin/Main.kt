7146d149221f125e2ad2e4ba2493643d70f97db8
Ponhrith <ponhrith.nhep@gmail.com>
2024-03-04 14:04:24 +0800

    TST-IntType

--- IntegerType/ByteShortIntLong/src/main/kotlin/Main.kt
+++ IntegerType/ByteShortIntLong/src/main/kotlin/Main.kt
@@ -1,9 +1,17 @@
 fun main() {
-    val number: Int = 22
+    var number = 22
 
     val maxIntegerValue: Int = Int.MAX_VALUE
     val minIntegerValue: Int = Int.MIN_VALUE
 
     println("Int maximum value is: $maxIntegerValue")
     println("Int minimum value is: $minIntegerValue")
+
+    number = 2147483647
+
+    val myMaxByteValue: Byte = Byte.MAX_VALUE
+    val myMinByteValue: Byte = Byte.MIN_VALUE
+    println("Byte maximum value is: $myMaxByteValue")
+    println("Byte minimum value is: $myMinByteValue")
+
 }
