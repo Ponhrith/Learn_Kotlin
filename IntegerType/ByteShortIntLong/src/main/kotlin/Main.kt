63f89bbdaf86bf24f1eb34c8e10817aa6aa559b9
Ponhrith <ponhrith.nhep@gmail.com>
2024-03-04 13:58:19 +0800

    TST-IntType

---
+++ IntegerType/ByteShortIntLong/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val number: Int = 22
+
+    val maxIntegerValue: Int = Int.MAX_VALUE
+    val minIntegerValue: Int = Int.MIN_VALUE
+
+    println("Int maximum value is: $maxIntegerValue")
+    println("Int minimum value is: $minIntegerValue")
+}
