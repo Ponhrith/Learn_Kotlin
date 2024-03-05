62602d95e5761adee7ee7b2bfd3168e3510a862f
Ponhrith <ponhrith.nhep@gmail.com>
2024-03-05 10:56:16 +0800

    TST-Login

--- Operators/src/main/kotlin/Main.kt
+++ Operators/src/main/kotlin/Main.kt
@@ -34,11 +34,19 @@ fun main() {
     println("x = ${x--}")
     println("x = ${--x}")
 
-    val isPlaying = true
-    val score = 80
-    if (isPlaying && score == 100){
-        println("Next level opened")
-    }else{
-        println("Still at the same level")
+//    val isPlaying = true
+//    val score = 80
+//    if (isPlaying && score == 100){
+//        println("Next level opened")
+//    }else{
+//        println("Still at the same level")
+//    }
+
+    val num1 = -5
+    val num2 = -3
+    if (num1 > 0 || num2 > 0){
+        println("The condition is true")
+    } else {
+        println("The condition is false")
     }
 }
