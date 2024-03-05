6d9b9cddcda88e0b2561d51916e5637167787622
Ponhrith <ponhrith.nhep@gmail.com>
2024-03-05 11:01:34 +0800

    TST-Logic

--- Operators/src/main/kotlin/Main.kt
+++ Operators/src/main/kotlin/Main.kt
@@ -44,12 +44,9 @@ fun main() {
 
     val num1 = -5
     val num2 = -3
-    val text = if (num1 > 0 || num2 > 0){
-        println("The condition is true")
+    val text = if (num1 > 0 || num2 > 0)
         "This is text 1"
-    } else {
-        println("The condition is false")
+    else
         "This is text 2"
-    }
     println("$text")
 }
